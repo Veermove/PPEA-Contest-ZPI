e5ef2f7fb5420d23f7a276a0b9b94a6d852b6754
Tym <75857174+Veermove@users.noreply.github.com>
2023-10-09 21:00:56 +0200

    [ZPI-7] Migrations adjustments (#8)
    * Basic grpc service
    
    * Adding database container
    
    * Basic connection to database
    
    * creating databse setup
    
    * Connection to database
    
    * Adding database schemas
    
    * Supporting migrations programmatically
    
    * minor fixes
    
    * Minor adjustments
    
    * Removing submission_id from person.jury_member
    
    * Dropping master_jury_id

--- data-store/db-client/migrations/20231007215129_edition.up.sql
+++ data-store/db-client/migrations/20231007215129_edition.up.sql
@@ -2,25 +2,25 @@ create schema edition;
 
 -- edycja_konkursu
 create table edition.contest (
-    contest_id     int generated always as identity primary key,
-    year           int not null,
-    master_jury_id int not null,
+    "contest_id"     int generated always as identity primary key,
+    "year"           int not null,
+    "master_jury_id" int not null,
 
-    est_time_individual_assessment  date,
-    est_time_preliminary_assessment date,
-    est_time_final_assessment       date,
-    est_time_jury_questions         date,
+    "est_time_individual_assessment"  date,
+    "est_time_preliminary_assessment" date,
+    "est_time_final_assessment"       date,
+    "est_time_jury_questions"         date,
 
-    min_project_duration_days     int not null,
-    min_participant_team_size     int not null,
-    min_subcontractors            int not null,
-    max_project_completion_months int not null,
+    "min_project_duration_days"     int not null,
+    "min_participant_team_size"     int not null,
+    "min_subcontractors"            int not null,
+    "max_project_completion_months" int not null,
 
-    url_code_of_conduct varchar(255),
-    url_schedule        varchar(255),
-    url_flyer           varchar(255),
-    url_finalists       varchar(255),
-    url_results         varchar(255),
+    "url_code_of_conduct" varchar(255),
+    "url_schedule"        varchar(255),
+    "url_flyer"           varchar(255),
+    "url_finalists"       varchar(255),
+    "url_results"         varchar(255),
 
     constraint master_jury_fk
         foreign key (master_jury_id) references person.jury_member(jury_member_id)
@@ -28,9 +28,9 @@ create table edition.contest (
 
 -- czlonek_jury_edycja_konkursu
 create table edition.jury_member_contest (
-    jury_member_contest_id int generated always as identity primary key,
-    jury_member_id         int not null,
-    contest_id             int not null,
+    "jury_member_contest_id" int generated always as identity primary key,
+    "jury_member_id"         int not null,
+    "contest_id"             int not null,
 
     constraint jury_member_contest_unique
         unique (jury_member_id, contest_id),
@@ -44,9 +44,9 @@ create table edition.jury_member_contest (
 
 -- przedstawieli_biura_nagrody_edycja_konkursu
 create table edition.awards_representative_contest (
-    awards_representative_contest_id int generated always as identity primary key,
-    awards_representative_id         int not null,
-    contest_id                       int not null,
+    "awards_representative_contest_id" int generated always as identity primary key,
+    "awards_representative_id"         int not null,
+    "contest_id"                       int not null,
 
     constraint awards_representative_contest_unique
         unique (awards_representative_id, contest_id),
@@ -61,12 +61,12 @@ create table edition.awards_representative_contest (
 
 -- kryterium_PEM
 create table edition.pem_criterion (
-    pem_criterion_id int  generated always as identity primary key,
-    contest_id       int  not null,
-    name             text not null,
-    description      text not null,
-    criteria         text not null,
-    subcriteria      text not null,
+    "pem_criterion_id" int  generated always as identity primary key,
+    "contest_id"       int  not null,
+    "name"             text not null,
+    "description"      text not null,
+    "criteria"         text not null,
+    "subcriteria"      text not null,
 
     constraint pem_criterion_contest_fk
         foreign key (contest_id) references edition.contest(contest_id)
