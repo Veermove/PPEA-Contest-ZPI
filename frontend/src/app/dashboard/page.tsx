e9f1f5d34fce379c77cec9eb4e58c1238e2128bc
gcfbn <bmekarski@interia.pl>
2023-10-23 22:56:59 +0200

    [ZPI-26] refactor the user dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -3,9 +3,10 @@
 import { useTranslation } from '@/app/i18n/client';
 import { useAuthContext } from '@/context/authContext';
 import { useRouter } from 'next/navigation';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { FaCommentDots, FaRegCheckCircle, FaRegListAlt, FaRegTimesCircle, FaUserPlus } from 'react-icons/fa';
 import { LuFolderEdit } from 'react-icons/lu';
+import DoubleTile from './doubleTile';
 
 function Dashboard() {
   const { t } = useTranslation('dashboard');
@@ -22,86 +23,44 @@ function Dashboard() {
 
   return (
     <Container className="py-3 mt-4">
-      <Row className="my-2 p-3">
-        <Col xs={6} className="btn btn-light btn-gray" onClick={() => handleItemClick("/")}>
-          <Row className="justify-content-around">
-            <Col xs={4}>
-              <div className="d-flex align-items-center h-100">
-                <h3 align="center" className="text-purple">{t('projectSubmissions')}</h3>
-              </div>
-            </Col>
-            <Col xs={2}>
-              <LuFolderEdit className="text-purple" size={72} />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={6} className="btn btn-light btn-gray" onClick={() => handleItemClick("/")}>
-          <Row className="justify-content-around">
-            <Col xs={4}>
-              <div className="d-flex align-items-center h-100">
-                <h3 align="center" className="text-purple">{t('initialRatings')}</h3>
-              </div>
-            </Col>
-            <Col xs={2}>
-              <FaRegTimesCircle className="text-purple" size={72} />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="my-2 p-3">
-        <Col xs={6} className="btn btn-light btn-gray" onClick={() => handleItemClick("/")}>
-          <Row className="justify-content-around">
-            <Col xs={4}>
-              <div className="d-flex align-items-center h-100">
-                <h3 align="center" className="text-purple">{t('PEMCriteria')}</h3>
-              </div>
-            </Col>
-            <Col xs={2}>
-              <FaRegListAlt className="text-purple" size={72} />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={6} className="btn btn-light btn-gray" onClick={() => handleItemClick("/")}>
-          <Row className="justify-content-around">
-            <Col xs={4}>
-              <div className="d-flex align-items-center h-100">
-                <h3 align="center" className="text-purple">{t('finalRatings')}</h3>
-              </div>
-            </Col>
-            <Col xs={2}>
-              <FaRegCheckCircle className="text-purple" size={72} />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="my-2 p-3">
-        <Col xs={6} className="btn btn-light btn-gray" onClick={() => handleItemClick("/")}>
-          <Row className="justify-content-around">
-            <Col xs={4}>
-              <div className="d-flex align-items-center h-100">
-                <h3 align="center" className="text-purple">{t('individualRatings')}</h3>
-              </div>
-            </Col>
-            <Col xs={2}>
-              <FaUserPlus className="text-purple" size={72} />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={6} className="btn btn-light btn-gray" onClick={() => handleItemClick("/")}>
-          <Row className="justify-content-around">
-            <Col xs={4}>
-              <div className="d-flex align-items-center h-100">
-                <h3 align="center" className="text-purple">{t('studyVisits')}</h3>
-              </div>
-            </Col>
-            <Col xs={2}>
-              <FaCommentDots className="text-purple" size={72} />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <DoubleTile
+        firstTileProps={{
+          text: t('projectSubmissions'),
+          Icon: LuFolderEdit,
+          onClick: () => handleItemClick("/")
+        }}
+        secondTileProps={{
+          text: t('initialRatings'),
+          Icon: FaRegTimesCircle,
+          onClick: () => handleItemClick("/")
+        }}
+      />
+      <DoubleTile
+        firstTileProps={{
+          text: t('PEMCriteria'),
+          Icon: FaRegListAlt,
+          onClick: () => handleItemClick("/")
+        }}
+        secondTileProps={{
+          text: t('finalRatings'),
+          Icon: FaRegCheckCircle,
+          onClick: () => handleItemClick("/")
+        }}
+      />
+      <DoubleTile
+        firstTileProps={{
+          text: t('individualRatings'),
+          Icon: FaUserPlus,
+          onClick: () => handleItemClick("/")
+        }}
+        secondTileProps={{
+          text: t('studyVisits'),
+          Icon: FaCommentDots,
+          onClick: () => handleItemClick("/")
+        }}
+      />
     </Container>
-  );
+  )
 }
 
 export default Dashboard;
