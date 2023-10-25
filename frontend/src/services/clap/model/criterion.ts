4316fcee8c0b54eab8c0dde15ad1b3e39083c70a
gcfbn <bmekarski@interia.pl>
2023-10-25 17:36:42 +0200

    [ZPI-27] add dummy clap API connection

---
+++ frontend/src/services/clap/model/criterion.ts
@@ -0,0 +1,8 @@
+export interface PEMCriterion {
+    pemCriterionId: number;
+    name: string;
+    description: string;
+    area: string;
+    criteria: string;
+    subcriteria?: string;
+}
---
+++ frontend/src/services/clap/model/rating.ts
@@ -0,0 +1,29 @@
+import { PEMCriterion } from "./criterion";
+import { AssessorsRatings } from "./submission";
+
+export enum RatingType {
+    INDIVIDUAL = "individual",
+    INITIAL = "initial",
+    FINAL = "final"
+}
+
+export interface Rating {
+    ratingId: number;
+    isDraft: boolean;
+    type: RatingType;
+}
+
+export interface PartialRating {
+    partialRatingId: number;
+    points: number;
+    justification: string;
+    lastModified: Date;
+}
+
+export interface RatingsDTO {
+    individualRatings: AssessorsRatings[];
+    initialRating?: AssessorsRatings;
+    finalRating?: AssessorsRatings;
+    criteria: PEMCriterion[];
+}
+
