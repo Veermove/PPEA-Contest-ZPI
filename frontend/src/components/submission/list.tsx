98e638ce9b2bec31d29825c83483d924d0e5af77
gcfbn <bmekarski@interia.pl>
2023-10-26 18:46:02 +0200

    [ZPI-27] Layout adjustments for the submission list

--- frontend/src/components/submission/list.tsx
+++ frontend/src/components/submission/list.tsx
@@ -5,37 +5,37 @@ import { Container, Row } from "react-bootstrap";
 import SubmissionOverview from "./overview";
 
 function splitSubmissionList(submissionList: SubmissionDTO[]): { activeSubmissions: SubmissionDTO[], archiveSubmissions: SubmissionDTO[] } {
-    return submissionList.reduce((result: { activeSubmissions: SubmissionDTO[], archiveSubmissions: SubmissionDTO[] }, submission) => {
-        result[submission.ratings.filter(rating => rating.type === RatingType.FINAL && rating.isDraft === false).length > 0 ? 'archiveSubmissions' : 'activeSubmissions'].push(submission)
-        return result;
-    }, { activeSubmissions: [], archiveSubmissions: [] })
+  return submissionList.reduce((result: { activeSubmissions: SubmissionDTO[], archiveSubmissions: SubmissionDTO[] }, submission) => {
+    result[submission.ratings.filter(rating => rating.type === RatingType.FINAL && rating.isDraft === false).length > 0 ? 'archiveSubmissions' : 'activeSubmissions'].push(submission)
+    return result;
+  }, { activeSubmissions: [], archiveSubmissions: [] })
 }
 
 function SubmissionList({ submissionList }: { submissionList: SubmissionDTO[] }) {
 
-    const {t} = useTranslation('submission/list');
+  const { t } = useTranslation('submission/list');
 
-    const { activeSubmissions, archiveSubmissions } = splitSubmissionList(submissionList);
-    return (
-        <Container>
-            <Row>
-                {!!activeSubmissions.length && (
-                    <>
-                        <h3>{t('activeSubmission')}</h3>
-                        <SubmissionOverview submission={activeSubmissions[0]} isActive={true} />
-                    </>
-                )}
-            </Row>
-            <Row>
-                {!!archiveSubmissions.length && (
-                    <>
-                        <h3>{t('archiveSubmissions')}</h3>
-                        {archiveSubmissions.map(submission => <SubmissionOverview submission={submission} isActive={false} />)}
-                    </>
-                )}
-            </Row>
-        </Container>
-    )
+  const { activeSubmissions, archiveSubmissions } = splitSubmissionList(submissionList);
+  return (
+    <Container className="mt-2">
+      <Row>
+        {!!activeSubmissions.length && (
+          <>
+            <h3>{t('activeSubmission')}</h3>
+            <SubmissionOverview submission={activeSubmissions[0]} isActive={true} />
+          </>
+        )}
+      </Row>
+      <Row className="mt-2">
+        {!!archiveSubmissions.length && (
+          <>
+            <h3>{t('archiveSubmissions')}</h3>
+            {archiveSubmissions.map(submission => <SubmissionOverview submission={submission} isActive={false} />)}
+          </>
+        )}
+      </Row>
+    </Container>
+  )
 }
 
 export default SubmissionList;
---
+++ frontend/src/components/submission/singleRating.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from "@/app/i18n/client";
+import { Rating } from "@/services/clap/model/rating";
+import { Col, Row } from "react-bootstrap";
+
+function SingleRating({ rating }: { rating: Rating }) {
+
+  const {t} = useTranslation('submission/overview');
+
+  return (
+    <Row className="text-black">
+      <Col xs={4}>{t(rating.type.toLocaleLowerCase())}:</Col>
+      <Col xs={8}><p className="font-bold">{rating.points}</p></Col>
+    </Row>
+  )
+}
+
+export default SingleRating;
