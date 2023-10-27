4d509d522a385e0dcc50545a6ee3242ef8d472f9
gcfbn <bmekarski@interia.pl>
2023-10-27 15:19:53 +0200

    [ZPI-27] translations + dummy data

--- frontend/src/components/submission/details.tsx
+++ frontend/src/components/submission/details.tsx
@@ -11,13 +11,6 @@ function SubmissionDetails({ submission, submissionDetails }: { submission: Subm
   const { t } = useTranslation('submission/details');
   const router = useRouter();
 
-  const testText =
-    `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non ex ut neque egestas sodales. Aliquam interdum libero et dui accumsan molestie. Integer tempor est quis hendrerit pellentesque. Fusce eget mattis diam. Duis molestie eros libero, eu pretium diam varius in. Aliquam dapibus arcu nisl. Phasellus dignissim tortor eu leo tristique hendrerit. Fusce at tincidunt dui. Nullam condimentum libero vel pellentesque maximus. Suspendisse sodales non orci vel cursus. Curabitur varius convallis sem, sed finibus purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec nisl lectus, aliquet quis lorem non, pulvinar congue libero. Morbi faucibus, elit ac lacinia posuere, leo velit maximus ante, vel porttitor nibh lacus eget metus. Vestibulum suscipit, eros eget venenatis laoreet, arcu odio mattis quam, eget sagittis risus nisi quis metus. Vestibulum egestas pretium elit ac dignissim.
-  
-  Pellentesque pretium lorem elit, in tempor nulla vestibulum eget. Nullam auctor quis ex nec scelerisque. Nam lorem dui, imperdiet eget leo sed, condimentum fermentum mi. Cras scelerisque ac est id feugiat. Pellentesque et risus nulla. Maecenas varius sapien vel leo lobortis, et iaculis ex pharetra. Ut massa nisi, eleifend id ipsum in, laoreet efficitur augue. Nulla facilisi. Cras dignissim quam sed diam pellentesque venenatis. Vestibulum. 
-  `
-
   return (
     <Container className="min-w-fit px-3 py-2">
       <Row>
@@ -31,26 +24,26 @@ function SubmissionDetails({ submission, submissionDetails }: { submission: Subm
       <Row>
         {/* <h1 className="text-center text-purple">{t('submissionHeader')} {submission.name}</h1> */}
         <Row>
-          <h1 className="text-left text-purple my-2">{t('submissionHeader')}: name</h1>
+          <h1 className="text-left text-purple my-2">{t('submissionHeader')} {submission.name}</h1>
         </Row>
       </Row>
       <Row className="mt-2 justify-content-between divide-x max-w-full">
         <Col xs={6} className="max-w-full">
-          <SubmissionDetail value="test" name="name" valueAlignment="center" />
-          <SubmissionDetail value="test" name="name" valueAlignment="left" />
-          <SubmissionDetail value="test" name="name" valueAlignment="right" />
-          <SubmissionDescription title="testDescription" description={testText} />
+          <SubmissionDetail value={submissionDetails.durationDays} name="durationDays" valueAlignment="center" />
+          <SubmissionDetail value={submissionDetails.teamSize.toString()} name="teamSize" valueAlignment="center" />
+          <SubmissionDetail value={submissionDetails.finishDate.toDateString()} name="finishDate" valueAlignment="center" />
+          <SubmissionDetail value={submissionDetails.budget} name="budget" valueAlignment="center" />
+          {!!submissionDetails.report?.reportSubmissionDate && <SubmissionDetail value={submissionDetails.report!.reportSubmissionDate!.toDateString()} name="reportSubmissionDate" valueAlignment="center" />}
+          <SubmissionDescription title="description" description={submissionDetails.description} />
         </Col>
         <Col xs={6} className="border-l-2 border-lightgray mr-0 max-w-full">
-          <SubmissionAttachment url={"/test"} name={"projectGoals"} />
-          <SubmissionAttachment url={"/test"} name={"projectGoals"} />
-          <SubmissionAttachment url={"/test"} name={"projectGoals"} />
-          <SubmissionAttachment url={"/test"} name={"projectGoals"} />
-          <SubmissionAttachment url={"/test"} name={"projectGoals"} />
-          <SubmissionAttachment url={"/test"} name={"projectGoals"} />
+          {!!submissionDetails.report?.projectGoals && <SubmissionAttachment url={submissionDetails.report.projectGoals} name={"projectGoals"} />}
+          {!!submissionDetails.report?.organisationStructure && <SubmissionAttachment url={submissionDetails.report.organisationStructure} name={"organisationStructure"} />}
+          {!!submissionDetails.report?.divisionOfWork && <SubmissionAttachment url={submissionDetails.report?.divisionOfWork!} name="divisionOfWork" />}
+          {!!submissionDetails.report?.projectSchedule && <SubmissionAttachment url={submissionDetails.report?.projectSchedule!} name="projectSchedule" />}
+          {!!submissionDetails.report?.attachements && <SubmissionAttachment url={submissionDetails.report?.attachements!} name="attachments" />}
         </Col>
       </Row>
-      {/* TODO it's scrolling horizontally */}
     </Container>
   )
 }
