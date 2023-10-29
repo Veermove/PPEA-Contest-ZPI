2b181e98f92115365802c158c59798fa24690aaf
veermove <75857174+Veermove@users.noreply.github.com>
2023-10-29 14:47:44 +0100

    Fix: GetSubmissions query not joining properly

--- backend/data-store/sql/gen/get_submissions_by_assessor.sql.go
+++ backend/data-store/sql/gen/get_submissions_by_assessor.sql.go
@@ -16,9 +16,11 @@ select
     submission.name          as "name",
     submission.duration_days as "duration_days"
 from project.assessor_submission assessor_submission
-inner join project.submission submission on submission.submission_id = assessor_submission.submission_id
-inner join edition.contest    contest    on contest.contest_id       = submission.contest_id
-inner join person.base        person     on person.person_id         = submission.person_id
+inner join project.submission submission  on submission.submission_id   = assessor_submission.submission_id
+inner join edition.contest    contest     on contest.contest_id         = submission.contest_id
+inner join person.assessor    assessor    on assessor.assessor_id       = assessor_submission.assessor_id
+inner join person.ipma_expert ipma_expert on ipma_expert.ipma_expert_id = assessor.ipma_expert_id
+inner join person.base        person      on person.person_id           = ipma_expert.person_id
 where
     person.email = $1
 `
