264bda54de47ff7e32f0ee3ecbff99f8a321a524
gcfbn <bmekarski@interia.pl>
2023-11-10 22:01:54 +0100

    [ZPI-40] adjust the data

--- frontend/src/app/assessor/ratings/ratingsFilter.ts
+++ frontend/src/app/assessor/ratings/ratingsFilter.ts
@@ -1,5 +1,6 @@
 import { RatingType, RatingsDTO } from "@/services/clap/model/rating";
 
+// TODO currently it doesn't work due to incorrect data from backend (responses should be null)
 export function isSubmissionRatingActive(ratings: RatingsDTO, submissionRatingType: RatingType) {
   switch (submissionRatingType) {
   case RatingType.INDIVIDUAL:
