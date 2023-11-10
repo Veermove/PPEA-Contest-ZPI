394379f21c62fa44a99c8991ce785fa3871485da
gcfbn <bmekarski@interia.pl>
2023-11-10 23:18:05 +0100

    Minor changes

--- backend/clap/src/main/java/zpi/ppea/clap/mappers/DtoMapper.java
+++ backend/clap/src/main/java/zpi/ppea/clap/mappers/DtoMapper.java
@@ -45,8 +45,6 @@ public interface DtoMapper {
     PartialRatingRequest dtoToPartialRatingRequest(UpdatePartialRatingDto dto, @Context Integer assessorId);
 
     default RatingsSubmissionResponseDto ratingsResponseToDto(RatingsSubmissionResponse ratings) {
-        final AssessorRatings initialRatings = ratings.getInitial();
-        final AssessorRatings finalRatings = ratings.getFinal();
         return RatingsSubmissionResponseDto.builder()
                 .criteria(DtoMapper.INSTANCE.criterionListToDto(
                         Optional.of(ratings.getCriteriaList()).orElse(List.of())
@@ -54,8 +52,8 @@ public interface DtoMapper {
                 .individualRatings(DtoMapper.INSTANCE.assessorRatingsListToDtos(
                         Optional.of(ratings.getIndividualList()).orElse(List.of())
                 ))
-                .initialRating(initialRatings != null ? DtoMapper.INSTANCE.assessorRatingsToDtos(initialRatings) : null)
-                .finalRating(finalRatings != null ? DtoMapper.INSTANCE.assessorRatingsToDtos(finalRatings) : null)
+                .initialRating(Optional.ofNullable(DtoMapper.INSTANCE.assessorRatingsToDtos(ratings.getInitial())).orElse(null))
+                .finalRating(Optional.ofNullable(DtoMapper.INSTANCE.assessorRatingsToDtos(ratings.getFinal())).orElse(null))
                 .build();
     }
 
