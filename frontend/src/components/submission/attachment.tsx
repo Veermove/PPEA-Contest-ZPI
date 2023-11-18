4120ded2abdc4db88eb388931be75df0e861e8d6
gcfbn <bmekarski@interia.pl>
2023-11-18 18:03:38 +0100

    [ZPI-36] introduce attachments

--- frontend/src/components/submission/attachment.tsx
+++ frontend/src/components/submission/attachment.tsx
@@ -1,9 +1,26 @@
 import { useTranslation } from "@/app/i18n/client";
+import { getFileLink } from "@/services/firebase/attachments/getAttachment";
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { FaPaperclip } from 'react-icons/fa';
 
-function SubmissionAttachment({ url, name }: { url: string, name: string }) {
+function SubmissionAttachment({ fileName, name, submissionId }: { fileName: string, name: string, submissionId: number }) {
   const { t } = useTranslation('submission/attachment');
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const firebaseUrl = await getFileLink(submissionId, fileName);
+        setUrl(firebaseUrl);
+      } catch (e) {
+        console.error(e);
+        setUrl('')
+      }
+    })();
+  }, [fileName, submissionId])
+
+
   return (
     <a href={url} className="no-underline visited:no-underline my-2 py-2">
       <Container className="py-3">
