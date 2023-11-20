3dd466ffc6e1b95fc89536ed31d7158c9e5476b6
remmo1 <remigiusz.pisarski@onet.pl>
2023-11-20 20:09:54 +0100

    [ZPI-55] - Add unit test for Partial Rating service

---
+++ backend/clap/src/test/java/zpi/ppea/clap/service/PartialRatingServiceTest.java
@@ -0,0 +1,43 @@
+package zpi.ppea.clap.service;
+
+import data_store.PartialRating;
+import data_store.UserClaimsResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import zpi.ppea.clap.dtos.UpdatePartialRatingDto;
+import zpi.ppea.clap.repository.PartialRatingRepository;
+import zpi.ppea.clap.security.FirebaseAgent;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PartialRatingServiceTest {
+    
+    @Mock
+    private PartialRatingRepository partialRatingRepository;
+
+    @InjectMocks
+    private PartialRatingService partialRatingService;
+
+    @Test
+    void upsertPartialRating() {
+        // given
+        FirebaseAgent.UserAuthData auth = new FirebaseAgent.UserAuthData(UserClaimsResponse.getDefaultInstance(), "false");
+        UpdatePartialRatingDto partialRatingDto = UpdatePartialRatingDto.builder()
+                .partialRatingId(1).criterionId(1).justification("ok").points(5).build();
+        PartialRating partialRating = PartialRating.newBuilder()
+                .setPartialRatingId(1).setModifiedBy(1).setJustification("ok").setCriterionId(1).setPoints(5).build();
+        when(partialRatingRepository.upsertPartialRating(partialRatingDto, auth)).thenReturn(partialRating);
+        
+        // when
+        var newPartialRating = partialRatingService.upsertPartialRating(partialRatingDto, auth);
+        
+        // then
+        assertThat(newPartialRating.getPartialRatingId()).isEqualTo(1);
+    }
+    
+}
