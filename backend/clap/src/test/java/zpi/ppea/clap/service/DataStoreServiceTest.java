d073df89985e50cce5a6048c85b66cf5c11c3c27
remmo1 <remigiusz.pisarski@onet.pl>
2023-11-26 18:33:24 +0100

    [ZPI-55] - Increase services coverage

--- backend/clap/src/test/java/zpi/ppea/clap/service/DataStoreServiceTest.java
+++ backend/clap/src/test/java/zpi/ppea/clap/service/DataStoreServiceTest.java
@@ -1,6 +1,7 @@
 package zpi.ppea.clap.service;
 
 import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.SettableFuture;
 import data_store.DataStoreGrpc;
 import data_store.UserClaimsResponse;
 import data_store.UserRequest;
@@ -9,9 +10,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import zpi.ppea.clap.exceptions.NoAccessToResource;
 import zpi.ppea.clap.security.FirebaseAgent;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,4 +42,22 @@ class DataStoreServiceTest {
         //then
         assertThat(newRating.getAssessorId()).isEqualTo(4);
     }
+
+    @Test
+    void getUserClaimsTest_grpcError() {
+        // given
+        FirebaseAgent.UserAuthData auth = new FirebaseAgent.UserAuthData(UserClaimsResponse.getDefaultInstance(), "false");
+        String email = "orlando.palladino@email.com";
+        UserClaimsResponse userClaim = UserClaimsResponse.newBuilder().setFirstName("Imię").setLastName("Nazwisko")
+                .setPersonId(11).setAssessorId(4).build();
+        UserRequest userRequest = UserRequest.newBuilder().setEmail(email).build();
+
+        SettableFuture<UserClaimsResponse> exceptionFuture = SettableFuture.create();
+        exceptionFuture.setException(new InterruptedException());
+        when(dataStoreFutureStub.getUserClaims(userRequest)).thenReturn(exceptionFuture);
+
+        // when + then
+        assertThrows(NoAccessToResource.class, () -> dataStoreService.getUserClaims(auth, email));
+    }
+
 }
