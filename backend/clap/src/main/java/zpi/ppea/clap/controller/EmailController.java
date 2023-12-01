0ecd561768daebd361726e31fa52d42bc3ef3853
remmo1 <remigiusz.pisarski@onet.pl>
2023-12-01 17:52:21 +0100

    [ZPI-73] - Add endpoint for emails

---
+++ backend/clap/src/main/java/zpi/ppea/clap/controller/EmailController.java
@@ -0,0 +1,23 @@
+package zpi.ppea.clap.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zpi.ppea.clap.mails.EmailServiceImpl;
+
+@RestController
+@RequestMapping("/admin/email")
+@AllArgsConstructor
+public class EmailController {
+
+    private final EmailServiceImpl emailService;
+
+    @GetMapping
+    public ResponseEntity<Void> upsertPartialRating() {
+        emailService.sendHtmlMessageFromFile();
+        return ResponseEntity.ok().build();
+    }
+
+}
